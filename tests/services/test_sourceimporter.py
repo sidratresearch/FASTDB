e57f6d07b21b8cb83a38ea0b8aab7d3c2ced5524
taiwithers <59430904+taiwithers@users.noreply.github.com>
2026-04-13 17:45:07 -0400

    Add calls to `test_user` fixture

--- tests/services/test_sourceimporter.py
+++ tests/services/test_sourceimporter.py
@@ -898,7 +898,7 @@ def test_import_only_30days_after_90days_consumed( import_only_30days_after_90da
 #   loading up a database for use developing the web ap.  See the developers documentation for FASTDB.
 
 @pytest.mark.skipif( env_as_bool('RUN_FULL90DAYS'), reason='RUN_FULL90DAYS is set' )
-def test_full90days_fast( alerts_90days_sent_received_and_imported, snana_fits_ppdb_loaded ):
+def test_full90days_fast( alerts_90days_sent_received_and_imported, snana_fits_ppdb_loaded, test_user):
     nobj, nroot, npos, nsrc, nfrc, ninfo = alerts_90days_sent_received_and_imported
     assert nobj == 37
     assert nroot == nobj
@@ -915,7 +915,7 @@ def test_full90days_fast( alerts_90days_sent_received_and_imported, snana_fits_p
 
 
 @pytest.mark.skipif( not env_as_bool('RUN_FULL90DAYS'), reason='RUN_FULL90DAYS is not set' )
-def test_full90days( fully_do_alerts_90days_sent_received_and_imported ):
+def test_full90days( fully_do_alerts_90days_sent_received_and_imported, test_user):
     nobj, nroot, npos, nsrc, nfrc, ninfo = fully_do_alerts_90days_sent_received_and_imported
     assert nobj == 37
     assert nroot == nobj
